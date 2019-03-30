285f2f45d7d9913c57c80382c98d096e4b60778b
Michal Bali <michalbali32@gmail.com>
2019-03-30 23:25:46 +0100

    tidy

--- as2/Server-Shared/client.cpp
+++ as2/Server-Shared/client.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 #include <omniORB4/CORBA.h>
 #include "master.h"
 
 using namespace std;
-
+using namespace std::chrono_literals;
 
 int main(int argc, char **argv) {
 	try {
@@ -14,7 +16,7 @@ int main(int argc, char **argv) {
 			return 0;
 		}
 
-		CORBA::String_var s = CORBA::string_dup("balim");
+		CORBA::String_var peer = CORBA::string_dup("balim");
 		CORBA::LongLong key;
 		if(argc == 3)
 		{
@@ -26,13 +28,26 @@ int main(int argc, char **argv) {
 
 		CORBA::Object_var server_base = orb->string_to_object(argv[1]);
 		master::server_i_var server = master::server_i::_narrow(server_base);
-
 		
 
-		master::instance_i_ptr inst = server->connect(s, key);
-
-		std::cout << s << "\n";
+		master::instance_i_ptr inst = server->connect(peer, key);
+		
+		std::cout << peer << "\n";
 		std::cout << key << "\n";
+		
+		
+		inst->ready(true);
+		
+		while(!inst->idle())
+			std::this_thread::sleep_for(1ms);
+		
+		std::cout << "ready\n";
+		
+		master::octet_sequence_t oct;
+		
+		master::count_t cnt = key;
+		inst->get_status(peer, cnt, oct);
+		
 		orb->destroy();
 
 	} catch (const CORBA::SystemException &ex) {
@@ -42,6 +57,10 @@ int main(int argc, char **argv) {
 	} catch (master::server_i::connection_e e) {
 		cerr << e.cause << endl;
 
+	}
+	catch(master::instance_i::protocol_e e)
+	{
+		cerr << e.cause << endl;
 	}
 	 catch (const exception &ex) {
 		cerr << "HERE2" << endl;
